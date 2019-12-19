5e67a54cce495f35f1fd474bca9a8033d8f549ab
Alex <alex_saranin@list.ru>
2019-12-20 02:59:41 +0300

    Update countBs

--- chapter_3/countBs.js
+++ chapter_3/countBs.js
@@ -1,11 +1,24 @@
 console.log(countBs('gfhBgjhgBkj'));
+console.log(countChar('gfhBgjhgBkj', 'B'));
 
-function countBs(str) {
+function countBs(str, char) {
     const l = str.length;
     let count = 0;
 
     for (let i = 0; i< l;i++){
         if (str[i] === 'B') {
+            count++;
+        }
+    }
+    return count;
+}
+
+function countChar(str, char) {
+    const l = str.length;
+    let count = 0;
+
+    for (let i = 0; i< l;i++){
+        if (str[i] === char) {
             count++;
         }
     }
